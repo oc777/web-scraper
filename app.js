fed0f29db12f1ccc8ed3fd7cab19d6cb24d103b2
Olga Christensen <oc222ba@student.lnu.se>
2018-02-02 13:03:55 +0100

    checking if arg is submitted

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 'use strict'
 
-console.log('hello world')
-
-// print process.argv
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val)
-})
-
 // get the url arg
 const url = process.argv[2]
 console.log('url: ' + url)
