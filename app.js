5c54835a1d1fa39a57b60f17cd95918d8f42fd0f
Olga Christensen <oc222ba@student.lnu.se>
2018-02-02 12:24:54 +0100

    retrieve url from process args

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+'use strict'
 
-console.log("hello world");
+console.log('hello world')
 
+// print process.argv
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val)
+})
 
+// get the url arg
+const url = process.argv[2]
+console.log('url: ' + url)
