b984d5013d24416f1201c34d7f16a740b204ef91
Olga Christensen <oc222ba@student.lnu.se>
2018-02-02 14:08:31 +0100

    testing 'fetch' module

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const calendar = require('./lib/calendar')
+
 // Check the arguments.
 let url = process.argv.slice(2)
 
@@ -7,3 +9,5 @@ if (url.length === 0) {
   console.log('ERROR: No URL provided.')
   process.exit(0)
 }
+
+calendar.scrape(url[0])
