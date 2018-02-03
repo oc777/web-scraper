f2dc06235c614c67f50b7bdf6e7a968f919b076a
Olga Christensen <oc222ba@student.lnu.se>
2018-02-03 13:00:09 +0100

    fetch promise

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const fetch = require('fetch').fetchUrl
+const dom = require('jsdom')
 const calendar = require('./lib/calendar')
 
 // Check the arguments.
@@ -10,4 +12,27 @@ if (url.length === 0) {
   process.exit(0)
 }
 
-calendar.scrape(url[0])
+// calendar, cinema, dinner
+let urls = []
+const getUrls = url => {
+
+}
+
+const get = url => {
+  return new Promise((resolve, reject) => {
+    fetch(url, (error, meta, body) => {
+      if (error) {
+        return reject(error)
+      }
+
+      return resolve([meta, body])
+    })
+  })
+}
+
+get(url[0]).then(value => {
+  console.log(value[0])
+  console.log(value[1].toString())
+})
+
+// calendar.scrape(url[0])
