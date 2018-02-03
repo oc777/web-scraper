19a60c14a70abd98b6766a2f7f062965aa3a473e
Olga Christensen <oc222ba@student.lnu.se>
2018-02-03 21:51:00 +0100

    moving getUrls to helpers.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 'use strict'
 
-const jq = require('cheerio')
-const client = require('./lib/client')
 const calendar = require('./lib/calendar')
+const helper = require('./lib/helpers')
 
 // Check that the url argument was provided
 let url = process.argv.slice(2)
@@ -13,22 +12,4 @@ if (url.length === 0) {
   process.exit(0)
 }
 
-/**
- * Gets the links to calendar, cinema and restaurant
- * and packs them into urls array
- *
- * @param {string} url
- * @returns {array} urls for calendar, cinema, dinner
- */
-const getUrls = async (url) => {
-  const values = await client.get(url)
-  const urls = []
-  const $ = await jq.load(values[1].toString())
-
-  $('a').map(function () {
-    urls.push($(this).attr('href'))
-  })
-  return urls
-}
-
-getUrls(url[0]).then(res => calendar.html(res[0]))
+helper.getUrls(url[0]).then(res => calendar.html(res[0]))
