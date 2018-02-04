025fbe9900d12af7f54844ee9a9b6d4fbcd4920f
Olga Christensen <oc222ba@student.lnu.se>
2018-02-04 23:39:28 +0100

    updating app.js; renaming entry functions

--- app.js
+++ app.js
@@ -14,20 +14,33 @@ if (url.length === 0) {
   process.exit(0)
 }
 
-/*
-// get urls of Calendar, Cinema, Dinner pages
-helper.getUrls(url[0])
-.then(res => calendar.html(res[0]))   // get friends' schedules
-.then(cal => console.log(cal))        // cal [{ name:'Paul', days:['ok','OK','--']},...]
-*/
+(async() => {
+  // get urls of Calendar, Cinema, Dinner pages
+  const urls = await helper.getUrls(url[0])
+
+  const calendarPromise = calendar.html(urls[0])
+  const cinemaPromise = cinema.movies(urls[1])
+  const dinnerPromise = dinner.restaurant(urls[2])
+
+  const[days, films, food] = await Promise.all([
+    calendarPromise,
+    cinemaPromise,
+    dinnerPromise
+  ])
+})()
 
 /*
-// cinema part
+// calendar
+.then(res => calendar.html(res[0]))   // get friends' schedules
+.then(cal => console.log(cal))        // cal [{ name:'Paul', days:['ok','OK','--']},...]
+
+// cinema
 helper.getUrls(url[0])
 .then(res => cinema.movies(res[1]))   //
 .then(cal => console.log(cal))        //
-*/
 
+// dinner
 helper.getUrls(url[0])
 .then(res => dinner.restaurant(res[2]))
 // .then(rest => console.log(rest))
+*/
